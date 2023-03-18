4896f0039a5f20cba5573fbfac684a3811193989
NLAarthy <126983197+NLAarthy@users.noreply.github.com>
2023-03-19 00:40:55 +0100

    Add files via upload
    Week 3 Days inheritance assignment

---
+++ Week3Day1/college/College.java
@@ -0,0 +1,25 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("Holycross");
+
+		// TODO Auto-generated method stub
+
+	}
+
+	public void collegeCode() {
+		System.out.println("641008");
+
+		// TODO Auto-generated method stub
+
+	}
+
+	public void collegeRank() {
+		System.out.println("first class");
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
