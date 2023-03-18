6a23770f8bc8248a3365f559f890a9eff9cf40f4
NLAarthy <126983197+NLAarthy@users.noreply.github.com>
2023-03-19 00:42:46 +0100

    Add files via upload
    Take home

---
+++ Week3Day1/ReverseString.java
@@ -0,0 +1,15 @@
+package w3day1;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "Amazon";
+		str = str.toLowerCase();
+		char[] ch = str.toCharArray();
+		for (int i = ch.length - 1; i >= 0; i--)
+			System.out.println(ch[i]);
+
+	}
+
+}
